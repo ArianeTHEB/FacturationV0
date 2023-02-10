bde43b1b9278b9526f89f318e87a01b88fe1a3d9
ArianeThe <ariane.thebault2022@campus-eni.fr>
2023-02-10 17:17:44 +0100

    04.Mise en place de la sécurité - création de l'entité User et mise à jour des fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
